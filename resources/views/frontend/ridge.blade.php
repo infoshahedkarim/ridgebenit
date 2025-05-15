f06e72399b1f38e02afd37d9e15418d9ebd1fb0a
Shahed Karim <info.shahedkarim@gmail.com>
2025-05-15 19:18:51 +0600

    testing

--- resources/views/frontend/ridge.blade.php
+++ resources/views/frontend/ridge.blade.php
@@ -982,7 +982,7 @@ User List
                         greatly improved our Furniture business through dynamic website. Thanks Ridgeben for their professionalism and
                         adaptability to last-minute changes.</p>
                      <div class="tptestimonial-avatar-info">
-                        <h5 class="tptestimonial-avatar-title">Md. Pabel Ahmed</h5>
+                        <h5 class="tptestimonial-avatar-title">Md. Rubel Ahmed</h5>
                         <span>Manager, Homeland Furniture</span>
                      </div>
                   </div>
